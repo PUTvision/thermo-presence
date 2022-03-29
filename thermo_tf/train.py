8cbe2fca1bce906dea7118411212f79e5530eede
Mateusz <piechocki.mateusz97@gmail.com>
2022-03-29 23:41:32 +0200

    Update train script

--- thermo_tf/train.py
+++ thermo_tf/train.py
@@ -8,12 +8,13 @@ import click
 import numpy as np
 import seaborn as sns
 import tensorflow as tf
-from tensorflow.keras.metrics import MeanAbsoluteError, MeanSquaredError
+from tensorflow.keras.metrics import CategoricalAccuracy
+from tensorflow_addons.metrics import F1Score
 import matplotlib.pyplot as plt
 import neptune.new as neptune
 from neptune.new.integrations.tensorflow_keras import NeptuneCallback
 
-from models import UNet
+from models import UNet, KraftUNet, MetwalyFNN
 from metrics import CountAccuracy, CountMAE, CountMSE, CountMeanRelativeAbsoluteError
 from data_generator.thermal_data_generator import ThermalDataset
 from data_generator.thermal_data_generator_v2 import ThermalDataset as ThermalDatasetv2
@@ -23,13 +24,8 @@ from utils.model_utils import check_model_prediction, evaluate
 
 @click.command()
 @click.option('-p', '--config_path', help='Path to file with config', type=str)
-@click.option('-f', '--in_out_filters', help='Input/Output filters number', type=int)
-@click.option('--batch_norm', help='Use batch normalization', type=bool)
-@click.option('--conv_transpose', help='Use conv_transpose layer', type=bool)
-@click.option('--squeeze', help='Squeeze more unet', type=bool)
-@click.option('--double_double_conv', help='Use twice double conv if unet is not squeezed', type=bool, default=True)
 @click.option('--log_neptune', help='Path to file with config', type=bool, default=True)
-def train(config_path, in_out_filters, batch_norm, conv_transpose, squeeze, double_double_conv, log_neptune):
+def train(config_path, log_neptune):
     print(f'TensorFlow version: {tf.__version__}')
     print(tf.config.list_physical_devices('GPU'))
 
@@ -37,6 +33,7 @@ def train(config_path, in_out_filters, batch_norm, conv_transpose, squeeze, doub
         config = yaml.safe_load(stream)
 
     model_save_name = config['model']['model_save_name']
+    density_estimation = config['model']['training_type'] == 'density_estimation'
 
     if log_neptune:
         with open('./configs/credentials.yaml', 'r') as stream:
@@ -47,14 +44,25 @@ def train(config_path, in_out_filters, batch_norm, conv_transpose, squeeze, doub
             api_token=credentials['neptune']['api_token']
         )
 
+        if config['model']['model_type'] == 'kraft_unet':
+            config['model']['batch_norm'] = True
+            config['model']['conv_transpose'] = True
+            config['model']['squeeze'] = True
+            config['model']['double_double_conv'] = True
+        elif config['model']['model_type'] == 'metwaly_fnn':
+            config['model']['batch_norm'] = False
+            config['model']['conv_transpose'] = False
+            config['model']['double_double_conv'] = False
+            config['model']['in_out_filters'] = 0
+
         run["model/config"] = {
-            "model_name": config['model']['model_save_name'],
+            "model_name": config['model']['model_type'],
             "input_shape": config['model']['input_shape'],
-            "batch_norm": batch_norm,
-            "conv_transpose": conv_transpose,
-            "squeeze": squeeze,
-            "double_double_conv": double_double_conv,
-            "filters": in_out_filters,
+            "batch_norm": config['model']['batch_norm'],
+            "conv_transpose": config['model']['conv_transpose'],
+            "squeeze": config['model']['squeeze'],
+            "double_double_conv": config['model']['double_double_conv'],
+            "filters": config['model']['in_out_filters'],
             "optimizer": config['model']['optimizer'],
             "loss": config['model']['loss'],
             "metrics": config['model']['metrics'],
@@ -88,43 +96,64 @@ def train(config_path, in_out_filters, batch_norm, conv_transpose, squeeze, doub
         augmented_data_test = AugmentedBatchesTrainingData()
         augmented_data_test.add_training_batch(test_data, flip_and_rotate=False)
 
-        train_data_gen = ThermalDataset(augmented_data_training, batch_size=config['model']['batch_size'])
-        val_data_gen = ThermalDataset(augmented_data_validation, batch_size=config['model']['batch_size'])
-        test_data_gen = ThermalDataset(augmented_data_test, batch_size=1)
+        train_data_gen = ThermalDataset(augmented_data_training, 
+                                        batch_size=config['model']['batch_size'],
+                                        task=config['model']['training_type'])
+        val_data_gen = ThermalDataset(augmented_data_validation, 
+                                      batch_size=config['model']['batch_size'],
+                                      task=config['model']['training_type'])
+        test_data_gen = ThermalDataset(augmented_data_test, batch_size=1,
+                                       task=config['model']['training_type'])
 
     elif config['dataset']['data_generator'] == 'v2':
         train_data_gen = ThermalDatasetv2(data_path=Path('./datasetv2'), sequences_names=config['dataset']["training_dirs"],
                                           person_point_weight=config['dataset']['sum_of_values_for_one_person'], 
-                                          batch_size=config['model']['batch_size'])
+                                          batch_size=config['model']['batch_size'], 
+                                          augment=True, task=config['model']['training_type'])
         val_data_gen = ThermalDatasetv2(data_path=Path('./datasetv2'), sequences_names=config['dataset']["validation_dirs"],
                                         person_point_weight=config['dataset']['sum_of_values_for_one_person'], 
-                                        batch_size=config['model']['batch_size'])
+                                        batch_size=config['model']['batch_size'], task=config['model']['training_type'])
         test_data_gen = ThermalDatasetv2(data_path=Path('./datasetv2'), sequences_names=config['dataset']["test_dirs"],
-                                         person_point_weight=config['dataset']['sum_of_values_for_one_person'], batch_size=1)
+                                         person_point_weight=config['dataset']['sum_of_values_for_one_person'], 
+                                         batch_size=1, task=config['model']['training_type'])
     else:
         raise ValueError(
             f"Unsupported version of data generator: {config['dataset']['data_generator']}")
 
-    model = UNet(
-        input_shape=config['model']['input_shape'],
-        in_out_filters=in_out_filters,
-        batch_norm=batch_norm,
-        conv_transpose=conv_transpose,
-        squeeze=squeeze,
-        double_double_conv=double_double_conv
-    )
+    if config['model']['model_type'] == 'kraft_unet':
+        model = KraftUNet(input_shape=config['model']['input_shape'], in_out_filters=config['model']['in_out_filters'])
+    elif config['model']['model_type'] == 'metwaly_fnn':
+        model = MetwalyFNN(input_shape=config['model']['input_shape'], squeeze=config['model']['squeeze'])
+    else:
+        model = UNet(
+            input_shape=config['model']['input_shape'],
+            in_out_filters=config['model']['in_out_filters'],
+            batch_norm=config['model']['batch_norm'],
+            conv_transpose=config['model']['conv_transpose'],
+            squeeze=config['model']['squeeze'],
+            double_double_conv=config['model']['double_double_conv']
+        )
 
-    model.compile(
-        optimizer=config['model']['optimizer'],
-        loss=config['model']['loss'],
+    metrics = config['model']['metrics']
+    if config['model']['training_type'] == 'density_estimation':
         metrics=[
-            MeanAbsoluteError(name='mae'),
-            MeanSquaredError(name='mse'),
+            *metrics,
             CountAccuracy(person_point_weight=config['dataset']['sum_of_values_for_one_person'], name='count_acc'),
             CountMAE(person_point_weight=config['dataset']['sum_of_values_for_one_person'], name='count_mae'),
             CountMSE(person_point_weight=config['dataset']['sum_of_values_for_one_person'], name='count_mse'),
             CountMeanRelativeAbsoluteError(person_point_weight=config['dataset']['sum_of_values_for_one_person'], name='count_mrae')
         ]
+    if config['model']['training_type'] == 'classification':
+        metrics = [
+            *metrics,
+            CategoricalAccuracy(name='accuracy'),
+            F1Score(num_classes=6, average='micro')
+        ]
+
+    model.compile(
+        optimizer=config['model']['optimizer'],
+        loss=config['model']['loss'],
+        metrics=metrics
     )
 
     if log_neptune:
@@ -154,53 +183,56 @@ def train(config_path, in_out_filters, batch_norm, conv_transpose, squeeze, doub
 
     model.save(f'./{model_save_name}.h5')
 
-    print('Test data:')
-    test_acc, test_f1, test_cm = evaluate(f'./{model_save_name}.h5', 'keras', test_data_gen, config)
-    print('Train data:')
-    train_acc, train_f1, train_cm = evaluate(f'./{model_save_name}.h5', 'keras', train_data_gen, config)
-    print('Validation data:')
-    val_acc, val_f1, val_cm = evaluate(f'./{model_save_name}.h5', 'keras', val_data_gen, config)
+    if density_estimation:
+        print('Test data:')
+        test_acc, test_f1, test_cm = evaluate(f'./{model_save_name}.h5', 'keras', test_data_gen, config)
+        print('Train data:')
+        train_acc, train_f1, train_cm = evaluate(f'./{model_save_name}.h5', 'keras', train_data_gen, config)
+        print('Validation data:')
+        val_acc, val_f1, val_cm = evaluate(f'./{model_save_name}.h5', 'keras', val_data_gen, config)
 
-    for cm, cm_name in [(test_cm, 'test'), (train_cm, 'train'), (val_cm, 'val')]:
-        plt.figure(figsize=(5, 4))
-        sns.heatmap(
-            cm, annot=True, cmap="Blues", fmt='0.0f',
-            xticklabels=np.arange(config['dataset']["max_people_count"]+1),
-            yticklabels=np.arange(config['dataset']["max_people_count"]+1)
-        )
-        plt.xlabel('Labels')
-        plt.ylabel('Predictions')
-        plt.savefig(f'./{cm_name}_confusion_matrix.png')
+        for cm, cm_name in [(test_cm, 'test'), (train_cm, 'train'), (val_cm, 'val')]:
+            plt.figure(figsize=(5, 4))
+            sns.heatmap(
+                cm, annot=True, cmap="Blues", fmt='0.0f',
+                xticklabels=np.arange(config['dataset']["max_people_count"]+1),
+                yticklabels=np.arange(config['dataset']["max_people_count"]+1)
+            )
+            plt.xlabel('Labels')
+            plt.ylabel('Predictions')
+            plt.savefig(f'./{cm_name}_confusion_matrix.png')
 
-    count, output_frame = check_model_prediction(model, config)
-    print(f'People count for raw frame: {round(count, 4)}')
+        count, output_frame = check_model_prediction(model, config)
+        print(f'People count for raw frame: {round(count, 4)}')
 
-    plt.figure(figsize=(16, 12))
-    plt.imshow(output_frame[0][..., 0])
-    plt.savefig('./output_frame.png')
+        plt.figure(figsize=(16, 12))
+        plt.imshow(output_frame[0][..., 0])
+        plt.savefig('./output_frame.png')
 
     if log_neptune:
         for i, metric in enumerate(eval_metrics):
             run["model/eval/{}".format(model.metrics_names[i])] = metric
 
         run["model/model"].upload(f'./{model_save_name}.h5')
-        run["model/eval/accuracy"] = test_acc
-        run["model/eval/F1"] = test_f1
-        run["model/eval/raw_confusion_matrix"] = test_cm
-        run["model/eval/confusion_matrix"].upload(f'./test_confusion_matrix.png')
-        run["model/eval/output_frame_count"] = count
-        run["model/eval/raw_output_frame"] = output_frame
-        run["model/eval/output_frame"].upload(f'./output_frame.png')
 
-        run["model/train/accuracy"] = train_acc
-        run["model/train/F1"] = train_f1
-        run["model/train/raw_confusion_matrix"] = train_cm
-        run["model/train/confusion_matrix"].upload(f'./train_confusion_matrix.png')
+        if density_estimation:
+            run["model/eval/accuracy"] = test_acc
+            run["model/eval/F1"] = test_f1
+            run["model/eval/raw_confusion_matrix"] = test_cm
+            run["model/eval/confusion_matrix"].upload(f'./test_confusion_matrix.png')
+            run["model/eval/output_frame_count"] = count
+            run["model/eval/raw_output_frame"] = output_frame
+            run["model/eval/output_frame"].upload(f'./output_frame.png')
 
-        run["model/val/accuracy"] = val_acc
-        run["model/val/F1"] = val_f1
-        run["model/val/raw_confusion_matrix"] = val_cm
-        run["model/val/confusion_matrix"].upload(f'./val_confusion_matrix.png')
+            run["model/train/accuracy"] = train_acc
+            run["model/train/F1"] = train_f1
+            run["model/train/raw_confusion_matrix"] = train_cm
+            run["model/train/confusion_matrix"].upload(f'./train_confusion_matrix.png')
+
+            run["model/val/accuracy"] = val_acc
+            run["model/val/F1"] = val_f1
+            run["model/val/raw_confusion_matrix"] = val_cm
+            run["model/val/confusion_matrix"].upload(f'./val_confusion_matrix.png')
 
     del model
 
